b3729d54d35025fbaf97ee7d8940d9a69a0e37cf
boteneric <eric@withpace.com>
2026-05-13 11:09:56 -0400

    disable public network access on ACR
    Satisfies Azure Policy 248 (GR-Initiative-CA: container registries
    must not allow unrestricted network access). Default azurerm config
    sets public_network_access_enabled = true, which the policy blocks
    at admission with 403 RequestDisallowedByPolicy.
    
    Disabling public access doesn't break the default connector deploy
    path (image is pulled from ghcr.io, not this ACR). Customers who
    want to mirror images here will need to add a private endpoint.

--- registry.tf
+++ registry.tf
@@ -1,7 +1,8 @@
 resource "azurerm_container_registry" "acr" {
-  name                = var.nuon_id
-  resource_group_name = data.azurerm_resource_group.rg.name
-  location            = data.azurerm_resource_group.rg.location
-  sku                 = "Premium"
-  admin_enabled       = false
+  name                          = var.nuon_id
+  resource_group_name           = data.azurerm_resource_group.rg.name
+  location                      = data.azurerm_resource_group.rg.location
+  sku                           = "Premium"
+  admin_enabled                 = false
+  public_network_access_enabled = false
 }
